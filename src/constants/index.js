41a5049c964ff4448a294fda5fe66f3a05ffa860
Safwan Sayeed <isafwansayeed@gmail.com>
2024-10-04 11:44:42 +0530

    Victory to Salute Emote change

--- src/constants/index.js
+++ src/constants/index.js
@@ -202,7 +202,7 @@ export const workExperiences = [
     duration: '2024- 1 Month',
     title: "Developed a web application using Django for the backend and Next.js for the frontend.The project features a user authentication system with registration and login capabilities. I implemented a calculator to assess the environmental impact and calculate the renewability factor for companies. Additionally, I designed and built the entire database from scratch, ensuring efficient data management and retrieval. This project highlights my skills in full-stack development and database management.",
     icon: './assets/framer.svg',
-    animation: 'victory',
+    animation: 'salute',
   },
   /*{
     id: 2,
@@ -220,6 +220,6 @@ export const workExperiences = [
     duration: 'Time',
     title: "Worked As",
     icon: './assets/yourpic.svg',
-    animation: 'salute',
+    animation: 'victory',
   },*/
 ];
