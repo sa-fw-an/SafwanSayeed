72eab07834452b26550dc16e7706cdd02861b280
sa-fw-an <isafwansayeed@gmail.com>
2024-11-24 23:05:46 +0530

    twitter to x minor changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -24,12 +24,12 @@ const Footer = () => {
 
           <a href="https://x.com/safwan_say" target="_blank" rel="noopener noreferrer" className="social-icon">
             <p className="hover:transform hover:scale-105 hover:translate-y-[-3px]  transition-transform duration-500 ease-in-out">
-              <img src="./assets/x.svg" alt="twitter" className="h-6 w-6" />
+              <img src="./assets/x.svg" alt="x" className="h-6 w-6" />
             </p>
           </a>
 
-          <div class="visit-counter-box flex items-center gap-2 bg-gray-800 text-gray-300 p-2 rounded-lg shadow-sm">
-            <span class="text-sm font-medium">🤵Portfolio Views</span>
+          <div className="visit-counter-box flex items-center gap-2 bg-gray-800 text-gray-300 p-2 rounded-lg shadow-sm">
+            <span className="text-sm font-medium">🤵Portfolio Views</span>
             <img src="https://hits.sh/safwansayeed.live.svg?style=round&label= &color=black" alt="visitor count" className="h-5 w-6" />
           </div>
 
