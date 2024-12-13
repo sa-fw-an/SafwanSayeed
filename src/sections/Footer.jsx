f765db676c25671da9079f849893ec215255c968
sa-fw-an <isafwansayeed@gmail.com>
2024-12-13 10:46:17 +0530

    counter fix

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
         <div className="visit-counter-box flex items-center gap-2 bg-gray-800 text-gray-300 p-1.5 rounded-lg shadow-sm mt-2 sm:mt-0 sm:ml-4">
           <span className="text-sm font-medium">🤵 Views</span>
           <img
-            src="https://hits.sh/test.com.svg?style=round&label= &color=black"
+            src="https://hits.sh/safwansayeed.live.svg?style=round&label= &color=black"
             alt="visitor count"
             className="h-5 w-6"
           />
